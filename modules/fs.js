0648ad76d3e9f78302becc53e6a324069f65165b
ana-zoom <ana.marianni@zoomtecnologia.com.br>
2025-05-24 12:26:30 -0300

    criando e modificando arquivos

--- modules/fs.js
+++ modules/fs.js
@@ -1,11 +1,47 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 //Criar pasta
-fs.mkdir(path.join(__dirname, '/test'), (error) => {
-    if(error) {
-        return console.log('Erro: ', error);
-    }
+fs.mkdir(path.join(__dirname, "/test"), (error) => {
+  if (error) {
+    return console.log("Erro: ", error);
+  }
 
-    console.log('Pasta criada com sucesso!');
+  console.log("Pasta criada com sucesso!");
 });
+
+//Criar arquivo
+fs.writeFile(
+  path.join(__dirname, "/test", "test.txt"),
+  "hello node!",
+  (error) => {
+    if (error) {
+      return console.log("Erro: ", error);
+    }
+    console.log("Arquivo criado com sucesso!");
+
+    //adicionar conteúdo a um arquivo
+    fs.appendFile(
+      path.join(__dirname, "/test", "test.txt"),
+      "hello world!",
+      (error) => {
+        if (error) {
+          return console.log("Erro: ", error);
+        }
+        console.log("Arquivo modificado com sucesso!");
+      }
+    );
+    
+    //Ler arquivos
+    fs.readFile(
+      path.join(__dirname, "/test", "test.txt"),
+      "utf8",
+      (error, data) => {
+        if (error) {
+          return console.log("Erro: ", error);
+        }
+        console.log(data);
+      }
+    );
+  }
+);
